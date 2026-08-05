adb4c17f8deaf733742879ab480bca381260f711
CECEP <cephiii94@users.noreply.github.com>
2026-08-05 16:47:18 +0800

    feat: Add Vercel configuration for Vite deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Optimal base path for Vercel deployment & custom domains
   define: {
     // Otomatis meng-generate Build Timestamp unik setiap kali `npm run build` dijalankan
     __APP_BUILD_TIME__: JSON.stringify(Date.now().toString())
